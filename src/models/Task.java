430bc45c6e007e4902fd64022bb548eacf508c91
akira <sonoda9311@outlook.com>
2020-03-02 22:24:14 +0900

    Modify Index/NewServlet

--- src/models/Task.java
+++ src/models/Task.java
@@ -3,6 +3,10 @@ package models;
 import java.sql.Timestamp;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
@@ -17,7 +21,9 @@ import javax.persistence.Table;
 @Table(name = "tasks")
 public class Task {
 
+    @Id
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "created_at", nullable = false)
